2c78e50ec5004c30e8f4ac150140b26870a4a479
Jim Borden <jim.borden@couchbase.com>
2024-03-02 06:31:27 +0900

    Add new Extensions interface package and fix winui test script

--- scripts/run_winui_tests.ps1
+++ scripts/run_winui_tests.ps1
@@ -1,6 +1,5 @@
 #Hard code the VS 2022 path due to VS2017 is also installed in the agent
-$VSInstall = "C:\Program Files\Microsoft Visual Studio\2022\Professional"
-#$VSInstall = (Get-CimInstance MSFT_VSInstance).InstallLocation
+$VSInstall = (& 'C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -requires Microsoft.NetCore.Component.SDK -requires Microsoft.NetCore.Component.Runtime.8.0 -property resolvedInstallationPath)
 if(-Not $VSInstall) {
     throw "Unable to locate Visual Studio installation"
 }
@@ -14,9 +13,9 @@ Remove-Item -Recurse -Force obj/ -ErrorAction Ignore
 Remove-Item -Recurse -Force bin/ -ErrorAction Ignore
 
 & $MSBuild --%  Couchbase.Lite.Tests.WinUI.sln /t:Restore /p:Configuration=Debug
-& $MSBuild --%  Couchbase.Lite.Tests.WinUI.sln /p:Configuration=Debug /p:Platform=x64 /p:TargetFramework=net6.0-windows10.0.19041.0 /p:AppxBundle=Never /p:UapAppxPackageBuildMode=SideloadOnly /p:PackageCertificateKeyFile=Couchbase.Lite.Tests.WinUI_TemporaryKey --no-restore
+& $MSBuild --%  Couchbase.Lite.Tests.WinUI.sln /p:Configuration=Debug /p:Platform=x64 /p:TargetFramework=net8.0-windows10.0.19041.0 /p:AppxBundle=Never /p:UapAppxPackageBuildMode=SideloadOnly /p:PackageCertificateKeyFile=Couchbase.Lite.Tests.WinUI_TemporaryKey --no-restore
 
-& $VSTest /InIsolation bin\x64\Debug\net6.0-windows10.0.19041.0\Couchbase.Lite.Tests.WinUI.dll /platform:x64 /Logger:trx /diag:out\diagnostic.txt
+& $VSTest /InIsolation bin\x64\Debug\net8.0-windows10.0.19041.0\Couchbase.Lite.Tests.WinUI.dll /platform:x64 /Logger:trx /diag:out\diagnostic.txt
 
 # Build and Test CBL CI nuget package 
 
@@ -24,6 +23,6 @@ Remove-Item -Recurse -Force obj/ -ErrorAction Ignore
 Remove-Item -Recurse -Force bin/ -ErrorAction Ignore
 
 & $MSBuild --%  Couchbase.Lite.Tests.WinUI.sln /t:Restore /p:Configuration=Release
-& $MSBuild --%  Couchbase.Lite.Tests.WinUI.sln /p:Configuration=Release /p:Platform=x64 /p:TargetFramework=net6.0-windows10.0.19041.0 /p:AppxBundle=Never /p:UapAppxPackageBuildMode=SideloadOnly /p:PackageCertificateKeyFile=Couchbase.Lite.Tests.WinUI_TemporaryKey
+& $MSBuild --%  Couchbase.Lite.Tests.WinUI.sln /p:Configuration=Release /p:Platform=x64 /p:TargetFramework=net8.0-windows10.0.19041.0 /p:AppxBundle=Never /p:UapAppxPackageBuildMode=SideloadOnly /p:PackageCertificateKeyFile=Couchbase.Lite.Tests.WinUI_TemporaryKey
 
-& $VSTest /InIsolation bin\x64\Release\net6.0-windows10.0.19041.0\Couchbase.Lite.Tests.WinUI.dll /platform:x64 /Logger:trx /diag:out\diagnostic.txt
+& $VSTest /InIsolation bin\x64\Release\net8.0-windows10.0.19041.0\Couchbase.Lite.Tests.WinUI.dll /platform:x64 /Logger:trx /diag:out\diagnostic.txt
